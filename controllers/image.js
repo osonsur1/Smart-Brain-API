7af0572d2f15307cefe0743b34b259985e29b6cc
Omkar Sonsurkar <osonsur1@binghamton.edu>
2019-06-10 05:31:18 -0400

    Add API

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: '5cc0c3a662404386bc50b71702d33b78'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
